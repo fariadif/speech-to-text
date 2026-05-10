2822138edcfa9e80cb151e47d14634ac2d4445c4
Fábio Faria Dias <fabiofariadias@hotmail.com>
2026-05-10 10:55:01 -0300

    Plot transcrition in the logger module

--- src/speech_to_text/modules/logger.py
+++ src/speech_to_text/modules/logger.py
@@ -6,4 +6,8 @@ class Logger(Module):
         self.subscribe("inference.transcription")
 
     def on_message(self, msg: Message):
-        print(f"[{self.name}] {msg.topic} @ {msg.timestamp:.2f} -> {msg.payload}")
+        if msg.topic == "inference.transcription":
+            text_segments = msg.payload["value"]
+
+            for seg in text_segments:
+                print(f"[{self.name}] {msg.topic} @ {msg.timestamp:.2f} -> {seg.text}")
