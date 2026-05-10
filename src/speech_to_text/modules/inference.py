b1720236193e93135cc667361084c1660a50752a
Fábio Faria Dias <fabiofariadias@hotmail.com>
2026-05-10 10:31:46 -0300

    Use the speech to text model to classify 5 second windows

--- src/speech_to_text/modules/inference.py
+++ src/speech_to_text/modules/inference.py
@@ -1,13 +1,47 @@
 from ..module import Module, Message
+from enum import Enum
+
+from pywhispercpp.model import Model
+
+
+class WhisperModel(Enum):
+    TINY = "ggml-tiny.bin"
+    BASE = "ggml-base.bin"
+    SMALL = "ggml-small.bin"
+    MEDIUM = "ggml-medium.bin"
+    LARGE = "ggml-large.bin"
 
 
 class Inference(Module):
+    MODEL_NAME = WhisperModel.BASE
+    MODEL_PATH = f"models/{MODEL_NAME.value}"
+
+    def _get_model(self):
+        import os
+        import urllib.request
+
+        if not os.path.exists(self.MODEL_PATH):
+            os.makedirs("models", exist_ok=True)
+
+            print(f"Downloading model {self.MODEL_NAME.value}")
+
+            urllib.request.urlretrieve(
+                f"https://huggingface.co/ggerganov/whisper.cpp/resolve/main/{self.MODEL_NAME.value}",
+                self.MODEL_PATH,
+            )
+
     def on_start(self):
+        self._get_model()
         self.subscribe("listener.audio")
 
-    def on_message(self, msg: Message):
+        self._model = Model(self.MODEL_PATH, language="en")
 
-        if msg.topic == "listener.audio":
-            print(
-                f"[{self.name}] {msg.topic} @ {msg.timestamp:.2f} -> Received an audio"
-            )
+    def on_message(self, msg: Message):
+        # if msg.topic == "listener.audio":
+        print(f"[{self.name}] {msg.topic} @ {msg.timestamp:.2f} -> Received an audio")
+
+        audio = msg.payload["value"]
+        segments = self._model.transcribe(audio.flatten())
+
+        for seg in segments:
+            print(f"[{self.name}] {seg.text}")
